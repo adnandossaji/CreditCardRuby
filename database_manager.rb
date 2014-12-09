a8889909ede17dbd27df5b77872cff81889dcc63
Adnan Dossaji <nan.dossaji@gmail.com>
2014-12-09 04:55:00 -0600

    DatabaseManager creates new database if one does not exist

--- database_manager.rb
+++ database_manager.rb
@@ -1,2 +1,23 @@
 require "sequel"
 
+if File.file?("database.db") == false
+
+	File.new("database.db", "w+")
+
+	DB = Sequel.sqlite('database.db')
+
+	DB.create_table :items do
+		primary_key :id
+		String :name
+		String :credit_card
+		Boolean :card_validity
+		Integer :limit
+		Integer :balance
+	end
+
+	@items = DB[:items]
+
+	puts "Made a database"
+
+end
+	
